4d48d75998cd2c5f94abf9b9975e7f7ee64fef30
EvaNaabye <evan19@ru.is>
2020-01-30 15:59:15 +0000

    Algorithm written for max_int.py

--- max_int.py
+++ max_int.py
@@ -8,15 +8,35 @@ total_cars = north_int + west_int + south_int + east_int
 while total_cars > 0:
     if (north_int + south_int) >= (east_int + west_int):
         print("Green lights on N/S")
-        north_int -= 5
-        south_int -= 5
-    total_cars -= (north_int - south_int)
+        if north_int >= 0 and north_int < 5:
+            total_cars -= north_int
+            north_int = 0
+        else:
+            total_cars -= 5
+            north_int -= 5
+
+        if south_int >= 0 and south_int < 5:
+            total_cars -= south_int
+            south_int = 0
+        else:
+            total_cars -= 5
+            south_int -= 5
 
     if (east_int + west_int) > (north_int + south_int):
         print("Green lights on E/W")
-        east_int -= 5
-        west_int -= 5
-    total_cars -= (west_int - east_int)
+        if east_int >= 0 and east_int < 5:
+            total_cars -= east_int
+            east_int = 0
+        else:
+            total_cars -= 5
+            east_int -= 5
+
+        if west_int >= 0 and west_int < 5:
+            total_cars -= west_int
+            west_int = 0
+        else:
+            total_cars -= 5
+            west_int -= 5
 
     if total_cars <= 0:
         print("No cars waiting, the traffic jam has been solved!")
